86bf0c81acff2cd8e04ad5269818e5c59197941a
jslby <me@jslby.ru>
2019-01-12 16:41:07 +0300

    add react

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HWP = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, '/src/index.js'),
+  output: {
+    filename: 'build.js',
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules:[
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets:['stage-0','react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins:[
+    new HWP(
+      {template: path.join(__dirname, '/src/index.html')}
+    )
+  ]
+}
