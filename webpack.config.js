f8ff519a4d37187e543f1f4b8c563b5a3a76e481
jslby <me@jslby.ru>
2019-01-13 23:30:25 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HWP = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.join(__dirname, '/src/index.js'),
+  performance: { hints: false },
   output: {
     filename: 'build.js',
     path: path.join(__dirname, '/dist'),
